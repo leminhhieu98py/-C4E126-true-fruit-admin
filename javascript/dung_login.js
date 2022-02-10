1e899866c71fd0fdc3827dd1a411735073ec0353
tiendung020 <tiendung020@gmail.com>
2022-02-10 21:24:05 +0700

    update code

---
+++ javascript/dung_login.js
@@ -0,0 +1,20 @@
+function Checklogin(event)
+{
+
+    
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+
+
+    if (username == '')
+    {
+
+        document.getElementById('username').style.backgroundColor = 'yellow';
+
+        innerHTML = 'Tên đăng nhập ko được để trống ';
+    }
+    
+
+}
