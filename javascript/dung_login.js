1f6f8c11d687942811d9f23c4abf025b095ed48c
tiendung020 <tiendung020@gmail.com>
2022-02-10 21:34:54 +0700

    HTML+CSS

--- javascript/dung_login.js
+++ javascript/dung_login.js
@@ -1,13 +1,7 @@
 function Checklogin(event)
 {
-
-    
-
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
-
-
-
     if (username == '')
     {
 
